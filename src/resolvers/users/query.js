5da0adb381ac75c882716f5e8145110f5a7d800b
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-01-08 05:51:05 +0700

    Update query.js

--- src/resolvers/users/query.js
+++ src/resolvers/users/query.js
@@ -1,6 +1,16 @@
 const users_services = require("./users_services");
 
 const usersQuery = {
+  me: async (_, args, context) => {
+    const users_id = context.auth.users._id;
+    const dto = { _id: users_id };
+
+    const result = await users_services.findOne(dto);
+    if (result.status_code === 1) {
+      return result.message;
+    }
+    return result;
+  },
   users: async (_, args) => {
     const { input } = args;
     const { page = 1, limit = 10 } = input;
