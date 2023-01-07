02e4ba588944eca8ca2f64d1b38aa677e1ddb309
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-01-07 23:44:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Fastify = require("fastify");
 const mercurius = require("mercurius");
 const fs = require("fs");
+const mercuriusAuth = require("mercurius-auth");
 const usersQuery = require("./src/resolvers/users/query");
 const usersMutations = require("./src/resolvers/users/mutations");
 const postsMutations = require("./src/resolvers/posts/mutations");
 const postsQuery = require("./src/resolvers/posts/query");
+const { verifyPaseto } = require("./src/resolvers/auth/auth_services");
 const schema = fs.readFileSync("./src/graphql/schema.graphql", "utf8");
 const app = Fastify();
 
@@ -23,6 +25,24 @@ app.register(mercurius, {
   schema,
   resolvers,
   graphiql: true,
+});
+
+app.register(mercuriusAuth, {
+  authContext(context) {
+    return {
+      atk: context.reply.request.headers["atk"],
+      def: context.reply.request.headers["def"],
+    };
+  },
+  async applyPolicy(authDirectiveAST, parent, args, context, info) {
+    const atk = context.auth.atk;
+    const def = context.auth.def;
+    if (atk && def) {
+      const decrypt = await verifyPaseto({ atk, def });
+      return decrypt;
+    }
+  },
+  authDirective: "auth",
 });
 
 const start = async () => {
