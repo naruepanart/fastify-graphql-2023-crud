dd4586532ba7dbcffc0a03ec4922750ce2b3ad50
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-01-08 02:08:42 +0700

    Update mutations.js

--- src/resolvers/posts/mutations.js
+++ src/resolvers/posts/mutations.js
@@ -1,25 +1,31 @@
 const posts_services = require("./posts_services");
 
 const postsMutations = {
-  createPosts: async (_, args) => {
+  createPosts: async (_, args, context) => {
     const { input } = args;
-    const result = await posts_services.create(input);
+    const users_id = context.auth.users._id;
+    const dto = { ...input, users: users_id };
+    const result = await posts_services.create(dto);
     if (result.status_code === 1) {
       return result.message;
     }
     return "Successfully inserted";
   },
-  updatePosts: async (_, args) => {
+  updatePosts: async (_, args, context) => {
     const { input } = args;
-    const result = await posts_services.update(input);
+    const users_id = context.auth.users._id;
+    const dto = { ...input, users: users_id };
+    const result = await posts_services.update(dto);
     if (result.status_code === 1) {
       return result.message;
     }
     return "Successfully updated";
   },
-  removePosts: async (_, args) => {
+  removePosts: async (_, args, context) => {
     const { input } = args;
-    const result = await posts_services.remove(input);
+    const users_id = context.auth.users._id;
+    const dto = { ...input, users: users_id };
+    const result = await posts_services.remove(dto);
     if (result.status_code === 1) {
       return result.message;
     }
@@ -28,9 +34,3 @@ const postsMutations = {
 };
 
 module.exports = postsMutations;
-
-/* const postsMutations = {
-  createPosts: async (_, args) => {},
-};
-
-module.exports = postsMutations;  */
